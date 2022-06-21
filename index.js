182cc63d54676ee621af3e928421031191786acd
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-20 19:45:50 -0700

    added to prompt questions

--- index.js
+++ index.js
@@ -1,2 +1,71 @@
 const inquirer = require(`inquirer`);
 const fs = require('fs');
+
+const readMe = ({data}) =>
+`<README.md>
+${title}
+***
+${description}
+***
+Table of Contents
+-Installation
+-Usage
+-Credits
+-License
+***
+${installation}
+***
+${usage}
+***
+${contributing}`
+
+
+
+
+
+
+
+
+
+inquirer
+.prompt([
+{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Describe your project?',
+    name: 'description',
+},
+// {
+//     type: 'input',
+//     message: 'tabel of contentns???.... ',
+//     name: 'tableContents',
+// },
+{
+    type: 'input',
+    message: 'What are the steps required to install your project?',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'Provide instructions and examples for use?',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'List your collaborators, if any, with links to their Github. If none type N/A',
+    name: 'contributing',
+},
+
+])
+
+.then((answers) => {
+    const readMeContent = readMe(answers);
+
+    fs.writeFile('README.md', readMeContent, (err) =>
+    err ? console.log(err) : console.log('file created!')
+    );
+});
